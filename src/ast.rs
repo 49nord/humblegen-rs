e4cf1fbd89406d9118e86f356a5abedfb360ac0f
Marc Brinkmann <git@marcbrinkmann.de>
2020-02-23 14:21:34 +0100

    Implement working Rust codegen, refactoring parser and ast into modules

---
+++ src/ast.rs
@@ -0,0 +1,80 @@
+#[derive(Debug)]
+pub struct Spec(pub Vec<SpecItem>);
+
+impl Spec {
+    pub fn iter(&self) -> impl Iterator<Item = &SpecItem> {
+        self.0.iter()
+    }
+}
+
+#[derive(Debug)]
+pub enum SpecItem {
+    StructDef(StructDef),
+    EnumDef(EnumDef),
+}
+
+#[derive(Debug)]
+pub struct StructDef {
+    pub name: String,
+    pub fields: StructFields,
+}
+
+#[derive(Debug)]
+pub struct StructFields(pub Vec<FieldNode>);
+
+impl StructFields {
+    pub fn iter(&self) -> impl Iterator<Item = &FieldNode> {
+        self.0.iter()
+    }
+}
+
+#[derive(Debug)]
+pub struct EnumDef {
+    pub name: String,
+    pub variants: Vec<VariantDef>,
+}
+
+#[derive(Debug)]
+pub struct VariantDef {
+    pub name: String,
+    pub variant_type: VariantType,
+}
+
+#[derive(Debug)]
+pub enum VariantType {
+    Simple,
+    Tuple(TupleDef),
+    Struct(StructFields),
+}
+
+#[derive(Debug)]
+pub struct FieldNode {
+    pub name: String,
+    pub type_ident: TypeIdent,
+}
+
+#[derive(Debug)]
+pub enum TypeIdent {
+    BuiltIn(AtomType),
+    List(Box<TypeIdent>),
+    Tuple(TupleDef),
+    UserDefined(String),
+}
+
+#[derive(Debug)]
+pub enum AtomType {
+    Str,
+    I32,
+    U32,
+    U8,
+    F64,
+}
+
+#[derive(Debug)]
+pub struct TupleDef(pub Vec<TypeIdent>);
+
+impl TupleDef {
+    pub fn components(&self) -> &Vec<TypeIdent> {
+        &self.0
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,193 +1,109 @@
-use pest::Parser;
-use pest_derive::Parser;
+use proc_macro2::TokenStream;
+use quote::quote;
 use std::fs;
 
-#[derive(Parser)]
-#[grammar = "humble.pest"]
-pub struct HumbleParser;
+mod ast;
+mod parser;
 
-#[derive(Debug)]
-struct Spec(Vec<SpecItem>);
+fn render_rust_spec(spec: &ast::Spec) -> TokenStream {
+    let items: Vec<_> = spec.iter().map(render_spec_item).collect();
 
-#[derive(Debug)]
-enum SpecItem {
-    StructDef(StructDef),
-    EnumDef(EnumDef),
+    quote!(
+        #(#items)*
+    )
 }
 
-#[derive(Debug)]
-struct StructDef {
-    name: String,
-    fields: StructFields,
+fn render_spec_item(spec_item: &ast::SpecItem) -> TokenStream {
+    match spec_item {
+        ast::SpecItem::StructDef(sdef) => render_struct_def(sdef),
+        ast::SpecItem::EnumDef(edef) => render_enum_def(edef),
+    }
 }
 
-#[derive(Debug)]
-struct StructFields(Vec<FieldNode>);
+fn render_enum_def(edef: &ast::EnumDef) -> TokenStream {
+    let ident = quote::format_ident!("{}", edef.name);
+    let variants: Vec<_> = edef.variants.iter().map(render_variant_def).collect();
 
-#[derive(Debug)]
-struct EnumDef {
-    name: String,
-    variants: Vec<VariantDef>,
+    quote!(
+        #[derive(Debug, Deserialize, Serialize)]
+        enum #ident { #(#variants),* }
+    )
 }
 
-#[derive(Debug)]
-struct VariantDef {
-    name: String,
-    variant_type: VariantType,
-}
+fn render_variant_def(vdef: &ast::VariantDef) -> TokenStream {
+    let ident = quote::format_ident!("{}", vdef.name);
 
-#[derive(Debug)]
-enum VariantType {
-    Simple,
-    Tuple(TupleDef),
-    Struct(StructFields),
-}
-
-#[derive(Debug)]
-struct FieldNode {
-    name: String,
-    type_ident: TypeIdent,
-}
-
-#[derive(Debug)]
-enum TypeIdent {
-    BuiltIn(AtomType),
-    List(Box<TypeIdent>),
-    Tuple(TupleDef),
-    UserDefined(String),
-}
-
-#[derive(Debug)]
-enum AtomType {
-    Str,
-    I32,
-    U32,
-    U8,
-    F64,
-}
-
-#[derive(Debug)]
-struct TupleDef(Vec<TypeIdent>);
-
-fn parse_struct_definition(pair: pest::iterators::Pair<Rule>) -> StructDef {
-    let mut nodes = pair.into_inner().into_iter();
-    let name = nodes.next().unwrap().as_span().as_str().to_string();
-    let fields = parse_struct_fields(nodes.next().unwrap());
-
-    StructDef { name, fields }
-}
-
-fn parse_struct_fields(pair: pest::iterators::Pair<Rule>) -> StructFields {
-    let fields: Vec<_> = pair
-        .into_inner()
-        .into_iter()
-        .map(parse_struct_field_def)
-        .collect();
-    StructFields(fields)
-}
-
-fn parse_enum_definition(pair: pest::iterators::Pair<Rule>) -> EnumDef {
-    let mut nodes = pair.into_inner().into_iter();
-    let name = nodes.next().unwrap().as_span().as_str().to_string();
-    let variants = nodes.map(parse_enum_variant_def).collect();
-
-    EnumDef { name, variants }
-}
-
-fn parse_enum_variant_def(pair: pest::iterators::Pair<Rule>) -> VariantDef {
-    let mut nodes = pair.into_inner().into_iter();
-    let name = nodes.next().unwrap().as_span().as_str().to_string();
-
-    if let Some(var) = nodes.next() {
-        match var.as_rule() {
-            Rule::struct_fields => VariantDef {
-                name,
-                variant_type: VariantType::Struct(parse_struct_fields(var)),
-            },
-            Rule::tuple_def => VariantDef {
-                name,
-                variant_type: VariantType::Tuple(parse_tuple_def(var)),
-            },
-            _ => unreachable!(dbg!(var)),
+    match vdef.variant_type {
+        ast::VariantType::Simple => quote!(#ident),
+        ast::VariantType::Tuple(ref tdef) => {
+            let inner = render_tuple_def(tdef);
+            quote!(#ident #inner)
         }
-    } else {
-        VariantDef {
-            name,
-            variant_type: VariantType::Simple,
+        ast::VariantType::Struct(ref fields) => {
+            let inner = render_struct_fields(fields);
+            quote!(#ident { #inner })
         }
     }
 }
 
-fn parse_struct_field_def(pair: pest::iterators::Pair<Rule>) -> FieldNode {
-    let mut nodes = pair.into_inner().into_iter();
-    let name = nodes.next().unwrap().as_span().as_str().to_string();
-    let type_ident = parse_type_ident(nodes.next().unwrap());
+fn render_struct_def(sdef: &ast::StructDef) -> TokenStream {
+    let ident = quote::format_ident!("{}", sdef.name);
+    let fields = render_struct_fields(&sdef.fields);
 
-    FieldNode { name, type_ident }
-}
-
-fn parse_type_ident(pair: pest::iterators::Pair<Rule>) -> TypeIdent {
-    let inner = pair.into_inner().into_iter().next().unwrap();
-    match inner.as_rule() {
-        Rule::built_in_atom => TypeIdent::BuiltIn(parse_built_in_atom(inner)),
-        Rule::list_type => parse_list_type(inner),
-        Rule::tuple_def => TypeIdent::Tuple(parse_tuple_def(inner)),
-        Rule::camel_case_ident => TypeIdent::UserDefined(inner.as_span().as_str().to_string()),
-        _ => unreachable!(dbg!(inner)),
-    }
-}
-
-fn parse_built_in_atom(pair: pest::iterators::Pair<Rule>) -> AtomType {
-    match pair.as_span().as_str() {
-        "str" => AtomType::Str,
-        "i32" => AtomType::I32,
-        "u32" => AtomType::U32,
-        "u8" => AtomType::U8,
-        "f64" => AtomType::F64,
-        _ => unreachable!(dbg!(pair)),
-    }
-}
-
-fn parse_list_type(pair: pest::iterators::Pair<Rule>) -> TypeIdent {
-    let inner = pair.into_inner().into_iter().next().unwrap();
-
-    TypeIdent::List(Box::new(parse_type_ident(inner)))
-}
-
-fn parse_tuple_def(pair: pest::iterators::Pair<Rule>) -> TupleDef {
-    TupleDef(
-        pair.into_inner()
-            .into_iter()
-            .map(parse_type_ident)
-            .collect(),
+    quote!(
+        #[derive(Debug, Deserialize, Serialize)]
+        struct #ident {
+            #fields
+        }
     )
 }
 
-fn parse_spec_item(pair: pest::iterators::Pair<Rule>) -> SpecItem {
-    match pair.as_rule() {
-        Rule::struct_definition => SpecItem::StructDef(parse_struct_definition(pair)),
-        Rule::enum_def => SpecItem::EnumDef(parse_enum_definition(pair)),
-        _ => unreachable!(dbg!(pair)),
+fn render_struct_fields(fields: &ast::StructFields) -> TokenStream {
+    let fields_rendered: Vec<_> = fields.iter().map(render_field_node).collect();
+    quote!(#(#fields_rendered),*)
+}
+
+fn render_field_node(field: &ast::FieldNode) -> TokenStream {
+    let name = quote::format_ident!("{}", field.name);
+    let ty = render_type_ident(&field.type_ident);
+
+    quote!(#name: #ty)
+}
+
+fn render_type_ident(type_ident: &ast::TypeIdent) -> TokenStream {
+    match type_ident {
+        ast::TypeIdent::BuiltIn(at) => render_atom_type(at),
+        ast::TypeIdent::List(inner) => {
+            let inner_rendered = render_type_ident(inner);
+            quote!(Vec<#inner_rendered>)
+        }
+        ast::TypeIdent::Tuple(tdef) => render_tuple_def(tdef),
+        ast::TypeIdent::UserDefined(id) => {
+            let ident = quote::format_ident!("{}", id);
+            quote!(#ident)
+        }
     }
 }
 
-// "parse spec"
-fn parse(input: &str) -> Spec {
-    let humbled = HumbleParser::parse(Rule::doc, input)
-        .unwrap()
-        .next()
-        .unwrap();
+fn render_atom_type(atom_type: &ast::AtomType) -> TokenStream {
+    match atom_type {
+        ast::AtomType::Str => quote!(String),
+        ast::AtomType::I32 => quote!(i32),
+        ast::AtomType::U32 => quote!(u32),
+        ast::AtomType::U8 => quote!(u8),
+        ast::AtomType::F64 => quote!(f64),
+    }
+}
 
-    Spec(
-        humbled
-            .into_inner()
-            .into_iter()
-            .map(parse_spec_item)
-            .collect(),
-    )
+fn render_tuple_def(tuple_def: &ast::TupleDef) -> TokenStream {
+    let components = tuple_def.components().iter().map(render_type_ident);
+
+    quote!((#(#components),*))
 }
 
 fn main() {
     let input = fs::read_to_string("src/sample.humble").unwrap();
-    dbg!(parse(&input));
+    let spec = parser::parse(&input);
+
+    println!("{}", render_rust_spec(&spec));
 }
