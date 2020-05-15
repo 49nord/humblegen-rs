6a7253996bb690896f2617bfc5727f6a0e380788
Christian Schwarz <me@cschwarz.com>
2020-05-15 11:22:41 +0200

    fixup 76024c91a369faf3494cb6a89bde967473589fb3 (address @mbr's review comments)

--- tests/rust.rs
+++ tests/rust.rs
@@ -2,6 +2,7 @@ use anyhow::Context;
 
 use std::path::PathBuf;
 
+#[derive(Debug)]
 struct RustTestCase {
     name: String,
     humble_spec: PathBuf,
@@ -11,8 +12,8 @@ struct RustTestCase {
 
 impl RustTestCase {
     fn run(&self) {
-        let spec_file = std::fs::read_to_string(&self.humble_spec).expect("read humble spec");
-        let spec = humblegen::parse_spec_str_or_panic(&spec_file);
+        let spec_file = std::fs::File::open(&self.humble_spec).expect("open humble spec file");
+        let spec = humblegen::parse(spec_file).expect("parse humble spec file");
         let spec_rust = humblegen::Language::Rust.render(&spec).to_string();
         // TODO rustfmt
         std::fs::write(&self.humble_rust_out, spec_rust).expect("write generated rust code");
@@ -26,7 +27,7 @@ impl RustTestCase {
     fn from_test_dir(dir: &std::fs::DirEntry) -> anyhow::Result<RustTestCase> {
         let entries: Vec<std::fs::DirEntry> = std::fs::read_dir(dir.path())?
             .collect::<std::io::Result<Vec<std::fs::DirEntry>>>()
-            .unwrap()
+            .context("read test dir entries")?
             .into_iter()
             .filter(|e| e.file_type().expect("get file type").is_file())
             .collect();
@@ -37,9 +38,27 @@ impl RustTestCase {
             .ok()
             .context("test case dir name must be a Rust String")?;
 
-        let mut humble_spec = None;
-        let mut humble_rust_out = None;
-        let mut main = None;
+        struct RequiredFile(Option<PathBuf>, &'static str, &'static str);
+        impl RequiredFile {
+            fn must_exist(self) -> anyhow::Result<PathBuf> {
+                self.0.clone().ok_or_else(|| {
+                    anyhow::anyhow!("test case dir requires file {:?} ({})", self.1, self.2)
+                })
+            }
+        }
+
+        let mut humble_spec = RequiredFile(None, "spec.humble", "input humble spec");
+        let mut humble_rust_out = RequiredFile(
+            None,
+            "spec.rs",
+            "reference output of Rust backend for spec.humble",
+        );
+        let mut main = RequiredFile(
+            None,
+            "main.rs",
+            "consumer of generated code (the test case)",
+        );
+        let mut required_files = vec![&mut humble_spec, &mut humble_rust_out, &mut main];
 
         for entry in entries {
             let name = entry
@@ -48,36 +67,35 @@ impl RustTestCase {
                 .ok()
                 .context("test case file names must be Rust strs")?;
 
-            match name.as_str() {
-                "main.rs" => main = Some(entry.path()),
-                "spec.humble" => humble_spec = Some(entry.path()),
-                "spec.rs" => humble_rust_out = Some(entry.path()),
-                x if x.starts_with(".") => (), // ignore
-                x if x.ends_with("~") => (),   // ignore
-                x => return Err(anyhow::anyhow!("unexpected file in test case dir: {:?}", x)),
+            for required_file in required_files.iter_mut() {
+                if required_file.1 == name.as_str() {
+                    required_file.0 = Some(entry.path());
+                }
             }
         }
 
         Ok(RustTestCase {
-            name: name.to_owned(),
-            humble_spec: humble_spec.unwrap(),
-            humble_rust_out: humble_rust_out.unwrap(),
-            main: main.unwrap(),
+            name: name.to_string(),
+            humble_spec: humble_spec.must_exist()?,
+            humble_rust_out: humble_rust_out.must_exist()?,
+            main: main.must_exist()?,
         })
     }
 }
 
 #[test]
 fn rust() {
+    // parse all the directories in ./tests/rust to RustTestCase instances
     let tests: Vec<RustTestCase> = std::fs::read_dir("./tests/rust/")
         .expect("read test dir")
         .collect::<std::io::Result<Vec<std::fs::DirEntry>>>()
-        .unwrap()
+        .expect("read test dir entries")
         .into_iter()
         .map(|dir_entry| RustTestCase::from_test_dir(&dir_entry))
         .collect::<anyhow::Result<Vec<RustTestCase>>>()
-        .unwrap();
+        .expect("parse test dirs as test cases");
 
+    // run them
     for test in tests {
         println!("running test {:?}", test.name);
         test.run();
