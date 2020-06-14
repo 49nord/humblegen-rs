3a67939ea4b6bd47ab3348ddc0761e21b2693a19
Reiner Dolp <770508+reiner-dolp@users.noreply.github.com>
2020-06-14 10:11:33 +0200

    [docgen] generate docs for user defined types

--- humblegen/src/backend/docs/script.js
+++ humblegen/src/backend/docs/script.js
@@ -1,19 +1,47 @@
+// Implements elements that can be folded using another element as handle, used to hide details of
+// endpoints and user defined types
 ;!function() {
-    var endpoints = Array.from(document.getElementsByClassName("endpoint"));
-    endpoints.forEach(endpoint => {
-        endpoint.getElementsByClassName("endpoint--method-and-route")[0].addEventListener("click", function(event) {
-            endpoint.classList.toggle("fold-open");
+    var foldables = Array.from(document.getElementsByClassName("foldable"));
+    foldables.forEach(foldable => {
+        foldable.getElementsByClassName("foldable-handle")[0].addEventListener("click", function(event) {
+            foldable.classList.toggle("fold-open");
         })
     });
 
     function unfoldAll() {
-        endpoints.forEach(endpoint => { endpoint.classList.add("fold-open"); event.preventDefault(); });
+        foldables.forEach(foldable => { foldable.classList.add("fold-open"); event.preventDefault(); });
     }
 
     function foldAll() {
-        endpoints.forEach(endpoint => { endpoint.classList.remove("fold-open"); event.preventDefault(); });
+        foldables.forEach(foldable => { foldable.classList.remove("fold-open"); event.preventDefault(); });
     }
 
     Array.from(document.getElementsByClassName("unfoldAll")).forEach(b => b.addEventListener("click", unfoldAll));
     Array.from(document.getElementsByClassName("foldAll")).forEach(b => b.addEventListener("click", foldAll));
+}();
+
+// Implements a tabs, a horizontal navigation switching between multiple elements below, used to switch between
+// languages in code snippets
+;!function() {
+    // initialize on page load, show first tab by default
+    var tabbedEls = Array.from(document.getElementsByClassName("tabs"));
+    tabbedEls.forEach(tabbed => {
+        var defaultActive = tabbed.getElementsByClassName("tabs-linked-group-selector")[0].dataset.tabGroup;
+
+        Array.from(document.getElementsByClassName("tabs-linked-group--"+defaultActive)).forEach(b => b.classList.add("tab-active"));
+        Array.from(document.getElementsByClassName("tabs-linked-group-selector--"+defaultActive)).forEach(b => b.classList.add("tab-active"));
+    });
+
+    tabbedEls.forEach(tabbed => {
+        Array.from(tabbed.getElementsByClassName("tabs-linked-group-selector")).forEach(tab => {
+            tab.addEventListener("click", function(event) {
+                 var linked = tab.dataset.tabGroup;
+                 Array.from(document.getElementsByClassName("tab-active")).forEach(b => b.classList.remove("tab-active"));
+                 Array.from(document.getElementsByClassName("tabs-linked-group--"+linked)).forEach(b => b.classList.add("tab-active"));
+                 Array.from(document.getElementsByClassName("tabs-linked-group-selector--"+linked)).forEach(b => b.classList.add("tab-active"));
+                 //tabItem.classList.toggle("fold-open");
+                 event.preventDefault();
+            })
+        })
+    })
 }();
