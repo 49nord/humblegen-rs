a23c1bf9136e96b5199a4be5ac0f3868a0ef25df
Reiner Dolp <770508+reiner-dolp@users.noreply.github.com>
2020-06-15 10:41:10 +0200

    [refactor] reenable build rs functionality

--- humblegen/src/lib.rs
+++ humblegen/src/lib.rs
@@ -1,6 +1,6 @@
 //! Humblegen compiler library
 
-use std::{fmt, io, path::Path};
+use std::{fmt, io, path::Path, path::PathBuf};
 
 pub use ast::Spec;
 
@@ -55,38 +55,29 @@ pub trait CodeGenerator {
     fn generate(&self, spec: &Spec, output: &Path) -> Result<(), LibError>;
 }
 
-// match self {
-//     Language::Rust => {
-//         let src = rust::render(&spec).to_string();
-//         rust::rustfmt::rustfmt_2018_generated_string(&src)
-//             .map(|s| s.into_owned())
-//             // if rustfmt doesn't work, use the unformatted source
-//             .unwrap_or(src)
-//     }
-//     Language::Elm => elm::render(&spec),
-//     Language::Docs => backend::docs::render(&spec),
-// }
-
-// /// This method is intended for use form within a `build.rs` file.
-// ///
-// /// Builds the specified humblefile using the Rust builder
-// /// and writes the generated code to `$OUT_DIR/protocol.rs`.
-// ///
-// /// Outputs `rerun-if-changed` instructions for the given `src` path.
-// pub fn build<P: AsRef<path::Path>>(src: P) -> Result<(), Box<dyn Error>> {
-//     println!("cargo:rerun-if-changed={}", src.as_ref().display());
-//     let out_dir: path::PathBuf = env::var("OUT_DIR").expect("read OUT_DIR envvar").into();
-//     let out_path = out_dir.join("protocol.rs");
-//     generate(std::fs::File::open(src)?, std::fs::File::create(&out_path)?)
-// }
-
-// pub fn generate<I: io::Read, O: io::Write>(src: I, mut dst: O) -> Result<(), Box<dyn Error>> {
-//     let rendered = Language::Rust.generate(&parse(src)?);
-//     Ok(dst.write_all(rendered.as_bytes())?)
-// }
-
 pub fn parse<I: io::Read>(mut src: I) -> Result<ast::Spec, LibError> {
     let mut input = String::new();
     src.read_to_string(&mut input).map_err(LibError::IoError)?;
     Ok(parser::parse(&input).map_err(LibError::ParseError)?)
+}
+
+/// This method is intended for use form within a `build.rs` file.
+///
+/// Builds the specified humblefile using the Rust builder
+/// and writes the generated code to `$OUT_DIR/protocol.rs`.
+///
+/// Outputs `rerun-if-changed` instructions for the given `src` path.
+pub fn build<P: AsRef<Path>>(src: P) -> Result<(), LibError> {
+    println!("cargo:rerun-if-changed={}", src.as_ref().display());
+    let out_dir: PathBuf = std::env::var("OUT_DIR")
+        .expect("read OUT_DIR envvar")
+        .into();
+    let out_path = out_dir.join("protocol.rs");
+
+    let infile = std::fs::File::open(src)?;
+    let spec = parse(infile)?;
+    let generator = backend::rust::Generator::new(Artifact::ServerEndpoints)?;
+    generator.generate(&spec, &out_path)?;
+
+    Ok(())
 }
