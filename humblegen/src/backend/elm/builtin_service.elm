76cd947cf01d7269bf292279254f7279978c9d1c
Reiner Dolp <770508+reiner-dolp@users.noreply.github.com>
2020-07-02 18:57:26 +0200

    [backend/elm] Fix case of prefix in authorization header

--- humblegen/src/backend/elm/builtin_service.elm
+++ humblegen/src/backend/elm/builtin_service.elm
@@ -95,7 +95,7 @@ expectRestfulJson toMsg clientVersion decoder =
 -- TODO: this code is obviously not portable to other projects, add auth annotations to humblegen
 withAuthorization : String -> Http.Header
 withAuthorization session = 
-    Http.header "Authorization" <| "CUSTOM " ++ session
+    Http.header "Authorization" <| "Custom " ++ session
 
 maybeWithAuthorization : Maybe String -> List Http.Header
 maybeWithAuthorization = 
