ecd261ffc152dec4c5c4d4b52124c81c8f81059c
Christian Schwarz <me@cschwarz.com>
2020-07-05 16:27:49 +0200

    [backend/elm] remove 'backend-version' checking code

--- humblegen/src/backend/elm/builtin_service.elm
+++ humblegen/src/backend/elm/builtin_service.elm
@@ -15,7 +15,6 @@ type alias VersionPairing =
 
 type Error =
       MissingOrInvalidAuth Http.Metadata
-    | ServerClientVersionMismatch Http.Metadata VersionPairing
     | OtherwiseBadStatus Http.Metadata -- most likely a server internal runtime error or bug in humblegen
     | NetworkProblems
     | Bug BugKind
@@ -30,20 +29,6 @@ type alias Response a = Result Error (Success a)
 requestId : Http.Metadata -> Maybe String
 requestId metadata =
     Dict.get "request-id" metadata.headers 
-
-serverVersion : Http.Metadata -> Maybe String
-serverVersion metadata =
-    Dict.get "backend-version" metadata.headers
-
-{-| Compare client and backend server versions, ignoring dirty worktrees.
--}
-eqVersions : VersionPairing -> Bool
-eqVersions version =
-  let
-    server = Maybe.map (String.replace "-modified" "") version.server
-    client = String.replace "-modified" "" version.client
-  in
-  server == Just client
 
 {-| Interpret response of a server
 
@@ -67,31 +52,18 @@ expectRestfulJson toMsg clientVersion decoder =
             -- Body of these responses is JSON with two fields: code and kind. Since
             -- we already know the code, and kind is just server internal stack-trace-like garbage neither
             -- frontend code nor end-user care about, discard...
-            let
-              maybeServerVersion = (serverVersion metadata)
-
-              versions = VersionPairing maybeServerVersion clientVersion
-            in if not (eqVersions versions) && maybeServerVersion /= Nothing then
-              Err <| ServerClientVersionMismatch metadata versions
-            else if metadata.statusCode == 401 then
+            if metadata.statusCode == 401 then
               Err <| MissingOrInvalidAuth metadata
             else
               Err <| OtherwiseBadStatus metadata
 
         Http.GoodStatus_ metadata body ->
-          -- TODO: note, we do not error if the http status is good, even though the server and client version mismatch.
-          -- not sure if this is a good idea or not? there are advantages and downsides
           case D.decodeString decoder body of
             Ok value ->
               Ok <| Success value metadata
 
             Err err ->
-              let
-                versions = VersionPairing (serverVersion metadata) clientVersion
-              in if not (eqVersions versions) then
-                Err <| ServerClientVersionMismatch metadata versions
-              else
-                Err <| Bug <| InvalidResponseBody metadata err
+              Err <| Bug <| InvalidResponseBody metadata err
 
 
 -- TODO: this code is obviously not portable to other projects, add auth annotations to humblegen
