717b2266dbf867f61039af7d298504eee544ab3a
Marc Brinkmann <git@marcbrinkmann.de>
2020-10-18 14:50:38 +0200

    Disable tests when building nix package

--- default.nix
+++ default.nix
@@ -3,4 +3,7 @@ pkgs.rustPlatform.buildRustPackage {
   name = "humblegen";
   src = ./.;
   cargoSha256 = "0jmiqvlg3lkx742vggnsnbwn27mh4kx722imlk1kyimq4d7dlsyz";
+
+  # Tests are currently disabled, as they spew quite a bit of output and may fail.
+  doCheck = false;
 }
